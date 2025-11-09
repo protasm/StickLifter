c60bc8375aee1bc98a436af72faa3538da015f42
Jurisware Developer <devel@jurisware.com>
2025-11-09 07:11:32 -0500

    Initial Commit

---
+++ StickLifterTests/StickLifterTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickLifterTests.swift
+//  StickLifterTests
+//
+//  Created by Jonathan Stroble on 11/9/25.
+//
+
+import Testing
+@testable import StickLifter
+
+struct StickLifterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
